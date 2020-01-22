eed107739895648ffd71187338f6c8fed7e988d9
Lúcás Meier <cronokirby@gmail.com>
2020-01-22 09:21:55 +0100

    Add sections page

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,9 +1,9 @@
 export default function Nav() {
   return (
-    <div className="px-48 py-2 flex items-baseline justify-around text-3xl font-bold text-white bg-teal-400">
-      <div className="hover:text-teal-200">Sections</div>
-      <div className="hover:text-teal-200">Documents</div>
-      <div className="hover:text-teal-200">Questions</div>
+    <div className="px-48 py-2 flex items-baseline justify-around text-3xl font-bold text-white bg-pink-400">
+      <div className="hover:text-pink-200">Sections</div>
+      <div className="hover:text-pink-200">Documents</div>
+      <div className="hover:text-pink-200">Questions</div>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,43 @@
+import fetch from 'isomorphic-unfetch';
 import Nav from '../components/Nav';
 
-export default function Home() {
+function Section({ section }: { section: string }) {
+  return <div key={section}>{section}</div>;
+}
+
+function NoSections() {
+  return (
+    <div className="w-1/2 mx-auto my-8 text-pink-800">
+      <h1 className="text-3xl font-bold">No Sections</h1>
+      <p className="my-2 text-lg">No sections have been created yet.</p>
+      <button className="px-4 py-1 mx-4 my-2 text-xl text-pink-100 bg-pink-400 rounded shadow-md hover:text-pink-400 hover:bg-pink-200">
+        Create
+      </button>
+    </div>
+  );
+}
+
+interface Props {
+  readonly sections: string[];
+}
+
+export default function Home({ sections }: Props) {
+  const showSections =
+    sections.length === 0 ? (
+      <NoSections />
+    ) : (
+      sections.map(s => <Section section={s} />)
+    );
   return (
     <>
-      <Nav /> <h1>Hello World</h1>
+      <Nav />
+      {showSections}
     </>
   );
 }
+
+Home.getInitialProps = async () => {
+  const res = await fetch('http://localhost:3000/api/sections');
+  const json = await res.json();
+  return { sections: json as string[] };
+};
